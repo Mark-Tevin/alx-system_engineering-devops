39d24af73b678a6093db1bc045886386f1250e9d
Mark-Tevin <tevinsmark@gmail.com>
2024-08-15 09:40:02 -0700

    queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,32 +4,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers (not
-    active users, total subscribers) for a given subreddit.
+    """Returns the number of subscribers for a given subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'My User Agent 1.0'}
 
-    if not a valid subreddit, return 0.
-    invalid subreddit may return to search results. Ensure that you are not
-    following redirects
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    Args:
-        subreddit (str): subreddit
-
-    Returns:
-        int: number of subscribers
-    """
-    base_url = 'https://www.reddit.com/r/'
-
-    url = '{}{}/about.json'.format(base_url, subreddit)
-    headers = {
-            'User-Agent':
-            'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-            Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
-    }
-    results = requests.get(
-        url,
-        headers=headers,
-        allow_redirects=False
-    )
-    if results.status_code == 200:
-        return results.json()['data']['subscribers']
-    return 0
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
