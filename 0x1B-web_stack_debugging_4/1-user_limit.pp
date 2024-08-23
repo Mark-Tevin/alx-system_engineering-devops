b0b7ceb0e4acbe2cac778633054308669cfd2659
Mark-Tevin <tevinsmark2gmail.com>
2024-08-23 20:06:29 +0000

    fix(os-config): enable holberton user login and file access without errors

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,11 +3,11 @@
 #Increase limit
 exec { 'increase-hard-limit-of-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000" /etc/security/limits.conf',
-  path    => '/bin/',
+  path    => '/usr/bin/:/bin/',
 }
 
 # Increase the limit for soft
 exec { 'increase-soft-limit-of-holberton-user':
   command => 'sed -i "/holberton soft/s/4/50000" /etc/security/limits.conf',
-  path    => '/bin/',
+  path    => '/usr/bin/:/bin/',
 }
